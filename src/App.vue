1535f04ff36ca1bbed81a7ac56b24158cfa8a5e5
Lee 哟 <205301508@qq.com>
2020-02-19 16:06:33 +0800

    登录页面整合

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
+    <router-view></router-view>
     <router-link to="/">点我啊</router-link>
+    <el-button>默认按钮</el-button>
   </div>
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,23 @@
 // 2. 导入插件
 import Vue from "vue"
-// 饿了吗
+// 
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
     Vue.use(ElementUI);
 
 import  VueRouter from 'vue-router'
+//  导入login 
+import login from '../views/login/login.vue'
 //3:在vue中全局使用vue-router需要注册
 Vue.use(VueRouter);
 
 //4.  实例化
 const router = new VueRouter({
   routes:[
-    
+    {
+        path:"/",
+        component:login
+    },
   ]
 })
 
