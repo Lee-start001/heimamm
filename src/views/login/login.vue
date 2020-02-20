367ba9bba13d422bfe9fc254be0f7c53ce1c7e15
Lee 哟 <205301508@qq.com>
2020-02-20 20:39:47 +0800

    登录页面布局完成

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,16 +1,102 @@
 <template>
   <div class="login-wrap">
     <div class="box1">
-      <span class="icon"></span>
-      <span class="heima">黑马面面</span>
-      <span class="line"></span>
+      <div class="title-box">
+        <img src="./images/login-icon9.png" alt />
+        <span class="heima">黑马面面</span>
+        <span class="line"></span>
+        <span class="mian">登录页面</span>
+      </div>
+      <!-- 表单 -->
+      <el-form :model="ruleForm" :rules="rules" ref="ruleForm">
+        <el-form-item prop="name">
+          <el-input v-model="ruleForm.name" placeholder="请输入手机号" prefix-icon="el-icon-search"></el-input>
+        </el-form-item>
+
+        <el-form-item prop="psw">
+          <el-input
+            v-model="ruleForm.psw"
+            show-password
+            placeholder="请输入密码"
+            prefix-icon="el-icon-lock"
+          ></el-input>
+        </el-form-item>
+
+        <el-form-item prop="code">
+          <el-row>
+            <el-col :span="17">
+              <el-input v-model="ruleForm.code" prefix-icon="el-icon-key" placeholder="请输入验证码"></el-input>
+            </el-col>
+            <el-col :span="7">
+              <img class="login-img" src="./images/login_code.png" alt />
+            </el-col>
+          </el-row>
+        </el-form-item>
+
+        <el-form-item   prop="checked">
+          <el-checkbox v-model="ruleForm.checked" class="agree" >
+            <span>
+              我已阅读并同意
+              <el-link type="primary">用户协议</el-link>和
+              <el-link type="primary">隐私条款</el-link>
+            </span>
+          </el-checkbox>
+        </el-form-item>
+
+        <el-form-item>
+          <el-button class="login-btn" type="primary" @click="submitForm('ruleForm')">登录</el-button>
+          <el-button class="login-btn" type="primary">注册</el-button>
+        </el-form-item>
+      </el-form>
     </div>
+
     <img src="./images/login_banner_ele.png" alt />
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      ruleForm: {
+        name: "",
+        psw: "",
+        code: "",
+        checked: "",
+       
+      },
+      rules: {
+        name: [{ required: true, message: "请输入账号", trigger: "blur" }],
+        psw: [{ required: true, message: "请输入密码", trigger: "blur" }],
+        code: [{ required: true, message: "请输入验证码", trigger: "blur" }],
+        checked:[
+          // 多选框没有失去焦点，只有值改变事件
+          // 因为checkbox其实他不可能值为空，除非你强行赋值为空
+          // 所以我们不能拿值是否为空来做验证了
+          // { required:true, message:'必须勾选同意用户协议',trigger:"change"},
+
+          // 只有值为true才满足条件，否则代表不匹配
+          { pattern:/true/, message:'必须勾选同意用户协议',trigger:"change"}
+        ],
+      }
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          alert("submit!");
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    }
+    // resetForm(formName) {
+    //   this.$refs[formName].resetFields();
+    // }
+  }
+};
 </script>
 
 <style lang="less">
@@ -30,32 +116,55 @@ export default {};
     width: 478px;
     height: 550px;
     background: rgba(245, 245, 245, 1);
-    padding-left: 48px;
-    overflow: hidden;
+    padding-top: 44px;
+    padding-left: 43px;
+    padding-right: 46px;
+    box-sizing: border-box;
 
-    .icon {
-      display: inline-block;
-      width: 22px;
-      height: 17px;
-      margin-top: 50px;
-      // background-color: #f00;
-      background-image: url("./images/login-icon9.png");
-      background-repeat: no-repeat;
+    // 给顶部题目一个盒子
+    .title-box {
+      display: flex;
+      align-items: center;
+      margin-bottom: 29px;
+
+      img {
+        width: 22px;
+        height: 17px;
+        margin-right: 16px;
+      }
+
+      .heima {
+        font-size: 24px;
+        margin-right: 16px;
+      }
+      .line {
+        width: 1px;
+        height: 28px;
+        background: rgba(199, 199, 199, 1);
+      }
+      .mian {
+        font-size: 22px;
+        margin-left: 12px;
+      }
     }
-    .heima {
-      width: 94px;
-      height: 22px;
-      font-size: 24px;
-      margin-left: 16px;
-    }
-    .line {
-      display: inline-block;
-      width: 1px;
-      height: 28px;
-      color: #ccc;
-      background-color: #ccc;
-      margin-left: 16px;
-      margin-top: 44px;
+  }
+
+  .agree {
+    display: flex;
+    align-items: center;
+  }
+
+  .login-img {
+    width: 100%;
+    height: 42px;
+    vertical-align: top;
+  }
+  .login-btn {
+    width: 100%;
+
+    &:nth-child(2) {
+      margin-left: 0px;
+      margin-top: 26px;
     }
   }
   // 登录页图片
