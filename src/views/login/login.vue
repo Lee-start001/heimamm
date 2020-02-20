d20940dd8304f59ed2eaf09e61b71592c2711ab2
Lee 哟 <205301508@qq.com>
2020-02-20 09:19:35 +0800

    登录页面布局

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="login-wrap">
-    <div class="box1"></div>
-    <img src="./images/login_banner_ele.png" alt="">
+    <div class="box1">
+      <span class="icon"></span>
+      <span class="heima">黑马面面</span>
+      <span class="line"></span>
+    </div>
+    <img src="./images/login_banner_ele.png" alt />
   </div>
 </template>
 
@@ -14,20 +18,46 @@ export default {};
   width: 100%;
   height: 100%;
   display: flex;
-  justify-content:space-around;
+  justify-content: space-around;
   align-items: center;
   background: linear-gradient(
     225deg,
     rgba(20, 147, 250, 1),
     rgba(1, 198, 250, 1)
   );
-   // 登录盒子
+  // 登录盒子
   .box1 {
     width: 478px;
     height: 550px;
     background: rgba(245, 245, 245, 1);
-  }
-   // 登录页图片
+    padding-left: 48px;
+    overflow: hidden;
 
+    .icon {
+      display: inline-block;
+      width: 22px;
+      height: 17px;
+      margin-top: 50px;
+      // background-color: #f00;
+      background-image: url("./images/login-icon9.png");
+      background-repeat: no-repeat;
+    }
+    .heima {
+      width: 94px;
+      height: 22px;
+      font-size: 24px;
+      margin-left: 16px;
+    }
+    .line {
+      display: inline-block;
+      width: 1px;
+      height: 28px;
+      color: #ccc;
+      background-color: #ccc;
+      margin-left: 16px;
+      margin-top: 44px;
+    }
+  }
+  // 登录页图片
 }
 </style>
