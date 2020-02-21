908da80fc115194cf2617245190763a2d7ff857e
Lee 哟 <205301508@qq.com>
2020-02-21 13:04:28 +0800

    完成注册页面布局

--- src/views/login/components/register.vue
+++ src/views/login/components/register.vue
@@ -1,14 +1,42 @@
 <template>
-  <el-dialog title="收货地址" :visible.sync="dialogFormVisible">
+  <el-dialog center title="用户注册" :visible.sync="dialogFormVisible" width='603px'>
     <el-form :model="form">
-      <el-form-item label="活动名称" :label-width="formLabelWidth">
+      <el-form-item label="昵称" :label-width="formLabelWidth">
         <el-input v-model="form.name" autocomplete="off"></el-input>
       </el-form-item>
-      <el-form-item label="活动区域" :label-width="formLabelWidth">
-        <el-select v-model="form.region" placeholder="请选择活动区域">
-          <el-option label="区域一" value="shanghai"></el-option>
-          <el-option label="区域二" value="beijing"></el-option>
-        </el-select>
+
+      <el-form-item label="邮箱" :label-width="formLabelWidth">
+        <el-input v-model="form.email" autocomplete="off"></el-input>
+      </el-form-item>
+
+      <el-form-item label="手机" :label-width="formLabelWidth">
+        <el-input v-model="form.phone" autocomplete="off"></el-input>
+      </el-form-item>
+
+      <el-form-item label="密码" :label-width="formLabelWidth">
+        <el-input v-model="form.password" autocomplete="off"></el-input>
+      </el-form-item>
+
+      <el-form-item label="图形码" :label-width="formLabelWidth">
+        <el-row>
+          <el-col :span="16">
+            <el-input v-model="form.piccode" autocomplete="off"></el-input>
+          </el-col>
+          <el-col :span="7" :offset='1'>
+            <img class="res_code" src="../images/login_code.png" alt />
+          </el-col>
+        </el-row>
+      </el-form-item>
+
+      <el-form-item label="验证码" :label-width="formLabelWidth">
+        <el-row>
+          <el-col :span="16">
+            <el-input v-model="form.rcode" autocomplete="off"></el-input>
+          </el-col>
+          <el-col :span="7" :offset='1'>
+            <el-button>获取用户验证码</el-button>
+          </el-col>
+        </el-row>
       </el-form-item>
     </el-form>
     <div slot="footer" class="dialog-footer">
@@ -33,11 +61,22 @@ export default {
         resource: "",
         desc: ""
       },
-      formLabelWidth: "120px"
+      formLabelWidth: "65px"
     };
   }
 };
 </script>
 
 <style>
+.el-dialog__header {
+  background: linear-gradient(
+    225deg,
+    rgba(2, 198, 252, 1),
+    rgba(22, 148, 253, 1)
+  );
+}
+.res_code {
+  width: 100%;
+  vertical-align: top;
+}
 </style>
